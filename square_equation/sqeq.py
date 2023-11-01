00cad2688a263f157a79d08795c561240b9a7011
georgekovin <108282258+georgekovin@users.noreply.github.com>
2023-11-01 15:57:13 +0300

    edit

--- square_equation/sqeq.py
+++ square_equation/sqeq.py
@@ -6,6 +6,7 @@ Simple function for quadratic equation.
 def square_equation(a: float, 
                     b: float = None, 
                     c: float = None, 
+                    real_only: bool = None,
                     round_to: int = None) -> tuple: 
     
     # documentation
@@ -19,57 +20,74 @@ def square_equation(a: float,
         a (float): quadratic coefficient
         b (float): linear coefficient
         c (float): constant coefficient
-        round_to (bool): rounding values if you need to
+        real_only (bool): returns real parts only if roots are complex numbers
+        round_to (int): rounding values if you need to
     
     Result
     ------
-        tuple: contains two values, float or complex
+        tuple: contains two roots of equation (x1, x2), float or complex
     
     """
-    
-    global result
-    global x1
-    global x2
+
     
     # if 'b' or 'c' values were forgotten, they are 0
     c = 0 if c is None else c
     
     if b is None:
-        x1 = (-c / a)**(1/2)
-        x2 = -((-c / a)**(1/2))
-        result = (x1, x2)
-        return result
+        x1 = (-c / a)**0.5
+        x2 = -x1
+        
+        return x1, x2
     
     # if 'a' equals to zero, the equation becomes linear
     if (a == 0) or (a is None):
-        x = -c / b
-        result = (x, x)
-        return result
-    
+        
+        if (b == 0) or (b is None):
+            x = c 
+        else:
+            x = -c / b
+            
+        return x, x
+
+
     # discriminant
     d = (b**2) - (4*a*c)
     
     if d == 0:
         x = -b / (2*a)
-        result = (x, x)
-        return result
+        return x, x
+    
     
     # final result
-    x1 = (-b + (d**(1/2))) / (2*a)
-    x2 = (-b - (d**(1/2))) / (2*a)
-    
+    x1 = (-b + (d**0.5)) / (2*a)
+    x2 = (-b - (d**0.5)) / (2*a)
+
     result = (x1, x2)
+    
+    
+    # boolean variable - does result contain complex numbers?
+    iscomplex = ((type(result[0]) == complex) and 
+                 (type(result[1]) == complex))
     
     # rounding values
     if round_to is not None:
-        if (type(result[0]) == complex) and (type(result[1]) == complex):
-            round_ = lambda x: complex(round(x.real, round_to), round(x.imag, round_to))
-            result = (round_(x1), round_(x2))
-            return result
+        if iscomplex:
+            round_ = lambda x: complex(round(x.real, round_to), 
+                                       round(x.imag, round_to))
             
-        elif (type(result[0]) == float) and (type(result[1]) == float):
+            result = (round_(x1), round_(x2))
+            
+        else:
             result = (round(x1, round_to), round(x2, round_to))
-            return result
     
-    return result
+    # return real part only
+    if (iscomplex) and (real_only is not None):
+        if round_to is not None:
+            result = (round(x1.real, round_to), 
+                      round(x2.real, round_to))
+        else:
+            result = (x1.real, x2.real)
+    
+    
+    return result 
 
